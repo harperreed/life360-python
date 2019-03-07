74f20b5397a98d4ffb5aa5986e2f584216215564
Phil Bruckner <pnbruckner@gmail.com>
2019-03-07 14:54:08 -0600

    Update setup.py
    Add dependencies.

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pnbruckner/life360",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
